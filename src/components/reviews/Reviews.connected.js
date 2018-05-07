cc05d27eb8dfba831066109a4f76ca331cad1170
carloluis <carloluisr@gmail.com>
2018-05-06 21:35:04 -0300

    ignore case when filtering..

--- src/components/reviews/Reviews.connected.js
+++ src/components/reviews/Reviews.connected.js
@@ -4,9 +4,14 @@ import { refreshReviews, fetchReviewsStart as fetchReviews } from './reviews.act
 import Reviews from './Reviews';
 
 const mapStateToProps = ({ reviews: { data, loading, hasMore, page, error }, search }) => {
+    const regex = new RegExp(search, 'i');
     const reviews = search
-        ? data.filter(review => review.title.includes(search) || review.content.includes(search))
+        ? data.filter(
+              ({ title, content, productTitle, childAsin }) =>
+                  regex.test(title) || regex.test(content) || regex.test(productTitle) || regex.test(childAsin)
+          )
         : data;
+
     return {
         reviews,
         loading,
