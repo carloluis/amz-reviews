9b450f4bc690bc385c6914e06fea1e5ed383e31e
carloluis <carloluisr@gmail.com>
2018-05-06 23:07:27 -0300

    update stars filter (daefault state) - star checked style

--- src/components/stars/stars.reducer.js
+++ src/components/stars/stars.reducer.js
@@ -1,6 +1,8 @@
 import { TOGGLE_STAR } from './stars.actions';
 
-function reducer(state = [], action) {
+const DEFAULT_STATE = [5];
+
+function reducer(state = DEFAULT_STATE, action) {
     if (action.type === TOGGLE_STAR) {
         const { star } = action;
         return state.includes(star) ? state.filter(item => item !== star) : [...state, star];
