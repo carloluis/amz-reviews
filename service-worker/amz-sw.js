8c7214b1c7b925fd14c6552e1fde90d7fe308c54
carloluis <carloluisr@gmail.com>
2018-05-29 01:09:09 -0300

    use fallback reviews whenever response is not ok

--- service-worker/amz-sw.js
+++ service-worker/amz-sw.js
@@ -8,18 +8,18 @@ self.addEventListener('fetch', event => {
     const [, page] = event.request.url.match(regexReviewsPage) || [];
     if (page) {
         event.respondWith(
-            fetch(event.request).catch(err => {
-                console.warn(`[ServiceWorker] reviews/${page}:`, err);
-
-                return getFallbackReviews().then(reviews => {
-                    const body = JSON.stringify(reviews[page - 1]);
-                    return new Response(body, {
-                        status: 200,
-                        statusText: 'OK',
-                        headers: { 'Content-Type': 'application/json' }
-                    });
-                });
-            })
+            fetch(event.request)
+                .then(res => {
+                    if (!res.ok) {
+                        console.warn(`[ServiceWorker] reviews/${page} with status ${res.status}`);
+                        return getResponse(page);
+                    }
+                    return res;
+                })
+                .catch(err => {
+                    console.warn(`[ServiceWorker] reviews/${page}:`, err);
+                    return getResponse(page);
+                })
         );
     }
 });
@@ -37,4 +37,15 @@ function getFallbackReviews() {
             reviews = data;
             return reviews;
         });
+}
+
+function getResponse(page) {
+    return getFallbackReviews().then(reviews => {
+        const body = JSON.stringify(reviews[page - 1]);
+        return new Response(body, {
+            status: 200,
+            statusText: 'OK',
+            headers: { 'Content-Type': 'application/json' }
+        });
+    });
 }
